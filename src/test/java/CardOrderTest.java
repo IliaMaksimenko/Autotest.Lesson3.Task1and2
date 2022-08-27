d8394f89769f0d2ddc002a9de79cb7726988a9f6
Илья Максименко <ilamaksimenko91@gmail.com>
2022-08-27 17:12:24 +0300

    Task1 shouldReceiveSuccessMessage

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class CardOrderTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void shouldReceiveSuccessMessage() {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Java\\WebDriverChromium\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Иванов Иван");
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("+79111111111");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("#root > div > div > p")).getText();
+
+        driver.quit();
+
+    }
+}
