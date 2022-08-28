05f65b425b3c68eb6c22a2adeb6fee1c7b32488f
Илья Максименко <ilamaksimenko91@gmail.com>
2022-08-28 13:52:46 +0300

    Fixed comment

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -43,32 +43,85 @@ public class CardOrderTest {
 
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Иванов Иван");
         driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("+79111111111");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
 
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("#root > div > div > p")).getText();
+        String actual = driver.findElement(By.xpath("//*[@data-test-id=\"order-success\"]")).getText();
 
         assertEquals(expected, actual);
 
     }
 
     @Test
-    public void shouldCheckValidation() {
+    public void shouldCheckValidationEmptyName() {
 
-        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Ivanov Ivan");
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("");
         driver.findElement(By.className("button__content")).click();
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("+79111111111");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        String expected = "Поле обязательно для заполнения";
 
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("#root > div > form > div:nth-child(1) > span > span > span.input__sub")).getText();
-
-        assertEquals(expected, actual);
+        assertEquals(expected,driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText());
 
     }
+
+
+    @Test
+    public void shouldCheckValidationEmptyPhone() {
+
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Иван Иванов");
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String expected = "Поле обязательно для заполнения";
+
+        assertEquals(expected,driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText());
+
+    }
+
+
+    @Test
+    public void shouldCheckValidationIncorrectName() {
+
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Ivan");
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("+79111111111");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+
+        assertEquals(expected,driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText());
+
+    }
+
+
+    @Test
+    public void shouldCheckValidationIncorrectPhone() {
+
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Иван Иванов");
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("791111111111");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        assertEquals(expected,driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText());
+
+    }
+
+
+    @Test
+    public void shouldCheckValidationCheckBox() {
+
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Иван Иванов");
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("+79111111111");
+        driver.findElement(By.className("button__content")).click();
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).click();
+
+    }
+
 }
